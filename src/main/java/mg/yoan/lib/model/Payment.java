0cf97033fcda843a54a689e734fb28ff7806c0d1
Rindra <hei.rindra.2@gmail.com>
2026-07-08 11:07:13 +0300

    style: code formated with format.sh

--- src/main/java/mg/yoan/lib/model/Payment.java
+++ src/main/java/mg/yoan/lib/model/Payment.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import java.math.BigDecimal;
 import java.time.Instant;
-import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.*;
 
