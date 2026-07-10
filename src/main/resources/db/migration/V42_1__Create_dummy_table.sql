990b22061f7bb8cd0381f19ea44cd38959b3d8d9
Yoan <yoandiny14@gmail.com>
2026-07-10 07:56:09 +0300

    emergency script fix

--- src/main/resources/db/migration/V42_1__Create_dummy_table.sql
+++ src/main/resources/db/migration/V42_1__Create_dummy_table.sql
@@ -1,9 +1,10 @@
 create table if not exists dummy
 (
     id varchar
-        constraint dummy_pk primary key
+    constraint dummy_pk primary key
 );
 
 insert into dummy (id)
 values ('dummy-table-id-1')
+    on conflict (id) do nothing
 ;
--- src/main/resources/db/migration/V42_2__Create_dummy_uuid_table.sql
+++ src/main/resources/db/migration/V42_2__Create_dummy_uuid_table.sql
@@ -1,9 +1,10 @@
 create table if not exists dummy_uuid
 (
     id varchar
-        constraint dummy_uuid_pk primary key
+    constraint dummy_uuid_pk primary key
 );
 
 insert into dummy_uuid (id)
 values ('dummy-uuid-id-1')
+    on conflict (id) do nothing
 ;
